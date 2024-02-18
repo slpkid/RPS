07cc9a3c650e8d6d27af44ce0c8fd0b630a40461
slpkid <153894144+slpkid@users.noreply.github.com>
2024-02-17 16:15:46 -0800

    Remove old code

--- main.js
+++ main.js
@@ -118,46 +118,6 @@ function newGame() {
     logMessage("New Game Started.")
 }
 
-
-// function playRound(playerSelection,computerSelection) { 
-//     computerSelection = getcomputerChoice(); 
-//     let resultRound //create variable to store won, loss, or tie
-//     if (playerSelection === computerSelection) { //check if playerSelection === computerSelection. If yes, the game is tied. 
-//         alert("Tie!"); //return "Tie!"
-//         return resultRound = "tie";
-//     } else if (playerSelection === "rock") { //else if player = rock
-//         if (computerSelection === "paper") {
-//             alert("You lose!")
-//             compScore++;
-//             return resultRound = "loss";
-//         } else {
-//             alert("You won!")
-//             pScore++;
-//             return resultRound = "won"
-//         }
-//     } else if (playerSelection === "scissors") { //else if player = scissors
-//         if (computerSelection === "rock") {
-//             alert("You lose!")
-//             compScore++;
-//             return resultRound = "loss";
-//         } else {
-//             alert("You won!")
-//             pScore++;
-//             return resultRound = "won";
-//         }
-//     } else if (playerSelection === "paper") { //else if player = paper
-//         if (computerSelection === "scissors") {
-//             alert("You lose!")
-//             compScore++;
-//             return resultRound = "loss";
-//         } else {
-//             alert("You won!")
-//             pScore++;
-//             return resultRound = "won";
-//         }
-//     }
-// }
-
 function getcomputerChoice() {
     //create variable compNum
     //get random number between 1 and 3
@@ -179,50 +139,3 @@ function getcomputerChoice() {
         return "scissors"
     }
 }
-
-
-
-// function playGame() {
-//     pScore = 0
-//     compScore = 0
-//     //create variable for gamewonLoss
-//     //let gamewonLoss
-//     //run playRound 5 times lmao
-//     console.log(playRound(playerSelection,computerSelection));
-//     console.log(playRound(playerSelection,computerSelection));
-//     console.log(playRound(playerSelection,computerSelection));
-//     console.log(playRound(playerSelection,computerSelection));
-//     console.log(playRound(playerSelection,computerSelection));
-//     //run a comparison between pScore and combuterScore
-//     //==> higher score wons. alert if player has won or lost or tied
-
-//     if (pScore > compScore) {
-//         alert("You beat the computer!")
-//         //return gamewonLoss = "Game won!"
-//     } else if (pScore < compScore) {
-//         alert ("You lost to the computer!")
-//        // return gamewonLoss = "Game loss!"
-//     } else if (pScore === compScore) {
-//         alert ("You tied the computer!") 
-//      //   return gamewonLoss = "Game tied!"
-//     }
-// }
-
-// //Data validation function:
-// //converts to lowercase first
-// //checks the for acceptable values for the user
-//     //has the user entered rock, paper, or scissors?
-//     //if yes, then nothing happens.
-// //otherwise it rejects the input.
-
-// //converts a string to lowercase
-// function stringLowerCase(a){
-//     return a = a.toLocaleLowerCase()
-// }
-// //create playerSelection constant
-// //prompt player for selection
-
-
-
-// //console.log(playRound(playerSelection,computerSelection));
-// //playGame();
